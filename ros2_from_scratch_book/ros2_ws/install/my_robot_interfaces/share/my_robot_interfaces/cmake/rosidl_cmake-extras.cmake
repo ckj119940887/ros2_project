d137deca858e0482b4593d55d4fdb304bf476652
kejun <ckj119940887@gmail.com>
2025-09-07 19:28:58 -0500

    service server

--- ros2_from_scratch_book/ros2_ws/install/my_robot_interfaces/share/my_robot_interfaces/cmake/rosidl_cmake-extras.cmake
+++ ros2_from_scratch_book/ros2_ws/install/my_robot_interfaces/share/my_robot_interfaces/cmake/rosidl_cmake-extras.cmake
@@ -1,4 +1,4 @@
 # generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
 
-set(my_robot_interfaces_IDL_FILES "msg/HardwareStatus.idl")
-set(my_robot_interfaces_INTERFACE_FILES "msg/HardwareStatus.msg")
+set(my_robot_interfaces_IDL_FILES "msg/HardwareStatus.idl;srv/ResetCounter.idl")
+set(my_robot_interfaces_INTERFACE_FILES "msg/HardwareStatus.msg;srv/ResetCounter.srv")
